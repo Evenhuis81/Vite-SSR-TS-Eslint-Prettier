f6a77e401374138c8422677729e8b961f001ad81
EvenhuisD <devenhuis81@gmail.com>
2022-12-15 16:21:12 +0100

    setup boilerplate

---
+++ renderer/PageShell.vue
@@ -0,0 +1,16 @@
+<template>
+    <slot />
+</template>
+
+<style>
+body {
+    margin: 0;
+    font-family: sans-serif;
+}
+* {
+    box-sizing: border-box;
+}
+a {
+    text-decoration: none;
+}
+</style>
